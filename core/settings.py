a53ff28742dcb42cbbdd65326a180260ee48ff8d
unknown <souidihaitam@outlook.com>
2026-05-19 18:37:54 +0200

    fix: remove hardcoded secret key fallback

--- core/settings.py
+++ core/settings.py
@@ -3,8 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-iw$d(xp=8*oaixk^5h_7h(hukqx(4iyi=awhxn2gsj!p6b@m*t')
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
