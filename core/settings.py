615771cc5155b15ea800a937ace27adaceb06f00
unknown <souidihaitam@outlook.com>
2026-06-11 21:00:02 +0200

    fix: force storage backend and include staticfiles

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'https://res.cloudinary.com/dw79oqzqb/image/upload/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
