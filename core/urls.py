2b540aac04c5270ea94f3530ac6f7c414fe6212e
unknown <souidihaitam@outlook.com>
2026-07-14 22:15:29 +0200

    Add Redis cache backend for rate limiting; fix Axes lockout parameters

--- core/urls.py
+++ core/urls.py
@@ -2,19 +2,16 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns # Import this for language-prefixed URLs
+from django.conf.urls.i18n import i18n_patterns
 
-# These patterns remain prefix-free (standard)
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('hci-portal/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
-     path('accounts/', include('allauth.urls')), # Logic for the language switcher
+    path('accounts/', include('allauth.socialaccount.urls')),
 ]
 
-# These patterns will automatically handle /fr/, /ar/, and /en/ prefixes
 urlpatterns += i18n_patterns(
-    path('', include('management.urls')), # Your H.C.I management logic
-    # If you have other apps like 'marketplace', include them here too
+    path('', include('management.urls')),
 )
 
 if settings.DEBUG:
